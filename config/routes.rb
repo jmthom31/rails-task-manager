cdc0574b5789ef31a9fbb7d796943e5e0215d613
Julia Thomas <juliathomas@outlook.com>
2021-11-09 12:24:58 +0000

    Added model and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # READ - all
+  get '/tasks', to: '#tasks#index'
+
+  # CREATE (new + create)
+  get '/tasks/:new', to: '#tasks#new', as: :new_task
+  post '/tasks', to: 'tasks#create'
+
+  # READ - one
+  get '/tasks/:id', to: '#tasks#show', as: :task
+
+  # UPDATE (edit + update)
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch '/tasks/:id', to: 'restaurants#save'
+
+  # DELETE
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
