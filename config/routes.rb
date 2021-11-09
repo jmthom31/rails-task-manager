9ef2d3ffbff323e6ac18fe427c9429222784cdb6
Julia Thomas <juliathomas@outlook.com>
2021-11-09 16:12:06 +0000

    tasks update

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # READ - all
-  get '/tasks', to: '#tasks#index'
+  get '/tasks', to: 'tasks#index'
 
   # CREATE (new + create)
-  get '/tasks/:new', to: '#tasks#new', as: :new_task
+  get '/tasks/new', to: 'tasks#new', as: :new_task
   post '/tasks', to: 'tasks#create'
 
   # READ - one
-  get '/tasks/:id', to: '#tasks#show', as: :task
+  get '/tasks/:id', to: 'tasks#show', as: :task
 
   # UPDATE (edit + update)
   get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-  patch '/tasks/:id', to: 'restaurants#update'
+  patch '/tasks/:id', to: 'tasks#update'
 
   # DELETE
   delete '/tasks/:id', to: 'tasks#destroy'
